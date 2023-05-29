ccbc6134f8c0bef247c531d3f0299f874a66572b
César <cesarsantossouza99@gmail.com>
2023-05-29 16:10:05 -0300

    em andamento

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+
+function App () {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,11 +14,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          cesar
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
