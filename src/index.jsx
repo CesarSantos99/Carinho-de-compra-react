55d871e97f6dc3d738b4860775b5996b87127f89
César <cesarsantossouza99@gmail.com>
2023-05-30 16:18:04 -0300

    em andamento

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import GlobalStyles from './styles/globalStyles'
+
+import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <App />
-    <GlobalStyles/>
   </React.StrictMode>
 )
 
