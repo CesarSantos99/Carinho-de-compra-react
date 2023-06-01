dda36bdb5fddf7e4c6e8df4f4aa911b62032fa35
César <cesarsantossouza99@gmail.com>
2023-06-01 16:07:15 -0300

    em andamento

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './Products.css'
+
+function Products() {
+
+  return (
+    <section className="products containe">
+      products
+    </section>
+  )
+}
+
+
+export default Products
