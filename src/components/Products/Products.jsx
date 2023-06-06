b1a3ffdadf178c009042722a35d0d502f47039a0
César <cesarsantossouza99@gmail.com>
2023-06-06 18:54:12 -0300

    product

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 import './Products.css'
+import fectchProducts from '../../api/fectchProduct'
+
 
 function Products() {
 
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+
+    fectchProducts('iphone').then((response) => {
+
+      setProducts(response)
+
+    })
+
+  },[])
+
+  console.log(products)
+
   return (
     <section className="products containe">
-      products
+      {
+        products.map((product) => <p key={product.title}>{product.title} </p>)
+      }
     </section>
   )
 }
