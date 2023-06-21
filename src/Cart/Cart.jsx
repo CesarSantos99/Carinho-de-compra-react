4c5bcf146254efbe1fd6b794e836f96092119b8e
César <cesarsantossouza99@gmail.com>
2023-06-21 16:24:58 -0300

    cart

---
+++ src/Cart/Cart.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Cart.css'
+
+function Cart() {
+  return (
+    <section className="Cart">
+      <div className="cart-items">items do carrinho</div>
+
+      <div className="cart-resume">resumo do carrinho</div>
+    </section>
+  )
+}
+
+export default Cart
