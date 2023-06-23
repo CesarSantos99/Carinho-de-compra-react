33a382ac29da0e5ddac9eaa10d3acfb827112627
César <cesarsantossouza99@gmail.com>
2023-06-23 18:27:52 -0300

    product

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,37 +1,32 @@
-import React, {useState, useEffect} from 'react'
+import React, {useEffect, useContext} from 'react'
 
 import './Products.css'
-import fectchProducts from '../../api/fectchProduct'
 import ProductsCard from '../ProductsCard/ProductsCard'
+import Loading from '../Loading/Loading'
+import fectchProducts from '../../api/fetchProduct'
+import AppContext from '../../context/AppContext'
 
 
 function Products() {
 
-  const [products, setProducts] = useState([])
+  const {products, setProducts, loading, setLoading} = useContext(AppContext)
 
   useEffect(() => {
 
     fectchProducts('iphone').then((response) => {
-
       setProducts(response)
+      setLoading(false)
 
     })
 
   },[])
 
-  console.log(products)
-
   return (
-    <section className="products containe">
 
-      {
-        products.map((product) => <ProductsCard key={product.id} data = {product}/>
-        )
-      }
-
-
-
-    </section>
+    (loading && <Loading/>) || (
+      <section className="products containe">
+        {products.map((product) => <ProductsCard key={product.id} data = {product}/>)}
+      </section>)  
   )
 }
 
